0da77aef362784c47503f857e419fb2b8bbd85d9
tokta <toktastuna@gmail.com>
2026-02-12 17:48:52 +0300

    out

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Bu satır Next.js'in statik HTML çıktı vermesini sağlar
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  images: {
+    unoptimized: true, // Statik exportta Next.js'in görsel optimizasyon motoru çalışmaz, bunu eklemelisin
+  },
 }
 
 export default nextConfig
